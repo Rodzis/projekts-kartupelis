46207367866c96702ad2b9410e23e7599a8db382
Rodzis <adamsonsrodrigo@gmail.com>
2025-01-23 13:57:09 +0200

    Add files via upload
    Izveidoju lauku

--- kartupelis.py
+++ kartupelis.py
@@ -1,1 +1,24 @@
-kartupelis = 1
+from tkinter import *
+from random import randint
+
+#laukums un kartupeļu lielumi
+LAUKUMS=10
+KARTUPEĻU_LIELUMI=[5,4,4,3,3,3,2,2,2,2]
+
+def izveidot_laukumu():
+    #izveido laukus no A līdz J
+    for col in range (LAUKUMS+1):
+        if col == 0:
+            Label(logs, text="", width=4, height=2).grid(row=0, column=col)
+        else:
+            Label(logs, text=chr(64 + col), width=4, height=2).grid(row=0, column=col)
+    #izveido laukus no 1 līdz 10
+    for row in range(1, LAUKUMS + 1):
+        Label(logs, text=str(row), width=4, height=2).grid(row=row, column=0)
+        for col in range(1, LAUKUMS + 1):
+            poga = Button(logs, text="", width=4, height=2, bg="gray", relief="solid", borderwidth=2,
+                               command=lambda r=row, c=col: minēt(r, c))
+            poga.grid(row=row, column=col)
+            
+logs=Tk()
+
